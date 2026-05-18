eb55c7a320da0177496902f609450db3e421f0f1
paredesalfredo94-coder <paredesalfredo94@gmail.com>
2026-05-18 15:37:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const pool = new Pool({
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 let pedidos = [];
 
@@ -23,6 +24,8 @@ app.get("/", (req, res) => {
 
 app.post("/pedido", (req, res) => {
 
+    console.log("BODY:", req.body);
+  
     const pedido = req.body;
 
     pedidos.push(pedido);
