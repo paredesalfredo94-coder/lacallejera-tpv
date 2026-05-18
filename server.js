b1aeac87619fdecee134edcfb218196df45cd4db
paredesalfredo94-coder <paredesalfredo94@gmail.com>
2026-05-18 14:21:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const { Pool } = require("pg");
 
 const app = express();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 app.use(cors());
 app.use(express.json());
